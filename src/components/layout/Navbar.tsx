7087e24209819ec46bcaf3639b7ae74639d26297
Kenzo Ferreira Fujimoto <155571101+kenzofujimoto@users.noreply.github.com>
2026-04-17 14:28:23 -0300

    logo da FCI

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -21,8 +21,8 @@ const navLinks = [
   { to: "/sobre", label: "Sobre", icon: Info },
 ] as const;
 
-const LOGO_URL = "https://www.mackenzie.br/fileadmin/CONFIGURACOES/DEFAULT_21/Resources/Public/Template/img/logo/mackenzie_w.svg";
-const LOGO_ALT = "[Instituto Presbiteriano Mackenzie]";
+const LOGO_URL = "/logo_FC_transparente.png";
+const LOGO_ALT = "[Faculdade de Computação e Informática - Mackenzie]";
 
 export default function Navbar() {
   const [mobileOpen, setMobileOpen] = useState(false);
